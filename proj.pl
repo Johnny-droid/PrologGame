c7c5ca1914d294de42735e02c026988f941e1e50
Johnny-droid <joaoricardoralves@gmail.com>
2022-12-27 22:20:45 +0000

    Completed display of Board (added example(1))

--- proj.pl
+++ proj.pl
@@ -1,3 +1,5 @@
+% GameState is a list of lists (9x9)
+
 play_game:-
     initial_state(GameState-Player),
     display_game(GameState-Player),
@@ -30,10 +32,67 @@ valid_moves(GameState, Moves):-
 
 choose_move(2, GameState, Moves, Move):-
     setof(Value-Mv, NewState^( member(Mv, Moves),
-    move(GameState, Mv, NewState),
-    evaluate_board(NewState, Value) ), [_V-Move|_]).
+        move(GameState, Mv, NewState),
+        evaluate_board(NewState, Value) ), [_V-Move|_]).
 
 
 
 
+
+% ------------------------- DISPLAY GAME --------------------------------
+
+display_game(GameState-Player) :-
+    display_header(),
+    display_game_aux(GameState-Player, 1).
+
+display_header() :-
+    write('    -------------------------------------'), nl,
+    write('    | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |'), nl,
+    write('-----------------------------------------'), nl.   
+
+display_game_aux([]-_, _) :- !.
+display_game_aux([Row | Tail]-Player, N) :-
+    format('| ~d | ', [N]),
+    display_row(Row), nl,
+    write('-----------------------------------------'), nl,
+    N1 is N+1,
+    display_game_aux(Tail-Player, N1).
+
+display_row([]) :- !.
+display_row([H | T]) :- 
+    display_cell(H), write(' | '),
+    display_row(T).
+
+display_cell(H) :-
+    (H = 0 -> write('.');
+    H = 1 -> write('X');
+    H = 2 -> write('O')).
+
+
+
+
+
+
+
+
+
+
+
+
+
+% ------------------------ GameState Examples ------------------------------
+
+
+example(1) :-
+    GameState = [[0,0,0,0,1,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,2],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,1,0,0,0,0,0,2],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,1,0,0]],
+    Player = human,
+    display_game(GameState-Player).
 
