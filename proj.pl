f6ef9764c4f50c544072cb45719e5ed13780cfa5
Johnny-droid <joaoricardoralves@gmail.com>
2022-12-28 15:08:18 +0000

    Solved valid_moves problem with instantiated values and changing player settings

--- proj.pl
+++ proj.pl
@@ -11,7 +11,7 @@ play :-
 play_game(Player1, Player2):-
     initial_state(GameState),
     display_game(GameState).
-    %game_cycle(GameState-Player).
+    %game_cycle(GameState-1).
 
 game_cycle(GameState-Player):-
     game_over(GameState, Winner), !,
@@ -27,27 +27,45 @@ game_cycle(GameState-Player):-
 
 
 % ------------------------- GAME LOGIC --------------------------------
-choose_move(GameState, human-X, Move):- % still needs to check if move is valid
+choose_move(GameState, human-X, Move, _):- % still needs to check if move is valid
     format('Player ~d turn!', [X]), nl,
     write('Enter row: '), read(Row),
     write('Enter column: '), read(Column),
     Move = Row-Column.
 
-choose_move(GameState, computer-Level, Move):-
+choose_move(GameState, computer-X, Move):-
     valid_moves(GameState, Moves),
-    choose_move(Level, GameState, Moves, Move).
+    choose_move_computer(Level, GameState, Moves, Move).
 
 valid_moves(GameState, Moves):-
-    findall(Move, move(GameState, Move, NewState), Moves).
+    findall(
+        Row-Column, 
+        (between(0,8,Row),
+        between(0,8,Column),
+        move(GameState, Row-Column, NewState)),
+        Moves).
 
-choose_move(1, _GameState, Moves, Move):-
+choose_move_computer(1, _GameState, Moves, Move):-
     random_select(Move, Moves, _Rest).
 
-choose_move(2, GameState, Moves, Move):-
+choose_move_computer(2, GameState, Moves, Move):-
     setof(Value-Mv, 
         NewState^( member(Mv, Moves),
         move(GameState, Mv, NewState),
         evaluate_board(NewState, Value) ), [_V-Move|_]).
+
+
+move(GameState, Row-Column, NewGameState):-
+    nth(Row, GameState, RowList),
+    nth(Column, RowList, 0),
+    replace(Column, RowList, 2, NewRowList),
+    replace(Row, GameState, NewRowList, NewGameState).
+    
+    
+replace(0, [_|T], X, [X|T]) :- !.
+replace(N, [H|T], X, [H|T1]) :-
+    N1 is N-1,
+    replace(N1, T, X, T1).
 
 
 game_over(GameState, Winner) :- 
@@ -62,19 +80,25 @@ display_main_menu :-
     write('|                                                 | \n'),  
     write('|                     CENTER                      | \n'),
     write('|                                                 | \n'),   
-    write('|               1. Player vs Player               | \n'),
-    write('|               2. Player vs Computer             | \n'), 
-    write('|               3. Computer vs Computer           | \n'), 
-    write('|               4. Exit                           | \n'), 
+    write('|              1. Player vs Player                | \n'),
+    write('|              2. Player vs Computer1             | \n'),
+    write('|              3. Player vs Computer2             | \n'), 
+    write('|              4. Computer1 vs Computer1          | \n'),
+    write('|              5. Computer1 vs Computer2          | \n'),
+    write('|              6. Computer2 vs Computer2          | \n'),
+    write('|              7. Exit                            | \n'), 
     write('|                                                 | \n'),  
     write('|                                                 | \n'),   
     write(' ------------------------------------------------- \n').  
 
 
-choose_main_menu(1):- play_game(human-1, human-2).   
-choose_main_menu(2):- play_game(human-1, computer-2).
-choose_main_menu(3):- play_game(computer-1, computer-2).
-choose_main_menu(4):- halt.
+choose_main_menu(1):- play_game(player(human,1,0), player(human,2,0)).   
+choose_main_menu(2):- play_game(player(human,1,0), player(computer,2,1)).
+choose_main_menu(3):- play_game(player(human,1,0), player(computer,2,2)).
+choose_main_menu(4):- play_game(player(computer,1,1), player(computer,2,1)).
+choose_main_menu(5):- play_game(player(computer,1,1), player(computer,2,2)).
+choose_main_menu(6):- play_game(player(computer,1,2), player(computer,2,2)).
+choose_main_menu(7):- halt.
 
 
 
